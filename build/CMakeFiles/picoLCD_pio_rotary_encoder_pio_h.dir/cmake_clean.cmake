7e0397888066385ce2ea56c03d69e0df37f7b6c9
Vitaris <viktor.tanko@gmail.com>
2022-11-25 22:17:39 +0100

    gitignore fixed

--- build/CMakeFiles/picoLCD_pio_rotary_encoder_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/picoLCD_pio_rotary_encoder_pio_h"
-  "pio_rotary_encoder.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/picoLCD_pio_rotary_encoder_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/stickerCutter_pio_rotary_encoder_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/stickerCutter_pio_rotary_encoder_pio_h"
-  "pio_rotary_encoder.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/stickerCutter_pio_rotary_encoder_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
