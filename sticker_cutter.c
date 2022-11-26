c7ece43049f9a9b224e1129469db43c17926304f
Vitaris <viktor.tanko@gmail.com>
2022-11-26 15:18:20 +0100

    Some speed ramp testing

--- sticker_cutter.c
+++ sticker_cutter.c
@@ -43,7 +43,7 @@ pidc_t pid_speed;
 
 // Control loop input,output and setpoint variables
 float input_speed = 0.0, output_speed = 0.0;
-float setpoint_speed = 750.0;
+float setpoint_speed = 12.5;
 
 // Control loop gains
 float kp_speed = 2.5, ki_speed = 1.5, kd_speed = 0.1;
@@ -56,7 +56,7 @@ pidc_t pid_pos;
 
 // Control loop input,output and setpoint variables
 float input_pos = 0.0, output_pos = 0.0;
-float setpoint_pos = 750.0;
+float setpoint_pos = 12.5;
 
 // Control loop gains
 float kp_pos = 2.5, ki_pos = 1.5, kd_pos = 0.1;
@@ -71,22 +71,35 @@ float diff_speed = 0.0;
 
 int *lcd_speed;
 
+// Positon Controler
+float a = 96.0;
+float ramp_time = 0.0;
+
 bool PID_timer_callback(struct repeating_timer *t) {
+    ramp_time += 0.001;
     // uint64_t t1 = time_us_64();
     enc_old = enc_new;
     enc_new = quadrature_encoder_get_count(pio_qEnc, sm_0);
     enc_dif = enc_new - enc_old;
     // lcd_speed = &enc_dif;
 
-    setpoint_speed = 750 + (sin(ramp) * 450);
+    // setpoint_speed = 12.5 + (sin(ramp) * 8.0);
     // setpoint = 1000;
-    input_speed = (enc_dif * 1000.0 / 4000.0) * 60.0;
-
-    
+    input_speed = (enc_dif * 1000.0 / 4000.0);
+    setpoint_speed = (ramp_time <= 24.0 / a) ? (a * ramp_time) : 24.0;
+ 
     pid_compute(pid_speed);
 
-    speed = (int)output_speed / 4.0;
+    speed = (int)output_speed * 15.5; // 15.5 = 1024 / 66.6 [ot/s]
     ramp += 0.008;
+
+    if (ramp_time > 1.5)
+    {
+        ramp_time = 0.0;
+        setpoint_speed = 0.0;
+    }
+        
+    
 
     pwm_set_chan_level(pwm_slice_0, PWM_CHAN_A, speed);
     pwm_set_chan_level(pwm_slice_0, PWM_CHAN_B, 1024 - speed); 
@@ -98,7 +111,7 @@ bool PID_timer_callback(struct repeating_timer *t) {
 float enc2speed(int enc){
     // 1000 - 1ms
     // 4000 - encoder tics per rev
-    return ((enc * 1000.0) / 4000.0) * 60.0;
+    return ((enc * 1000.0) / 4000.0);
 }
 
 bool LCD_timer_callback(struct repeating_timer *t) {
@@ -131,14 +144,14 @@ int main() {
     // Prepare PID controller for operation
     pid_speed = pid_create(&ctrldata_speed, &input_speed, &output_speed, &setpoint_speed, kp_speed, ki_speed, kd_speed);
     // Set controler output limits from 0 to 200
-    pid_limits(pid_speed, 0, 4000);
+    pid_limits(pid_speed, 0, 66);
     // Allow PID to compute and change output
     pid_auto(pid_speed);
 
     // Position PID
     pid_pos = pid_create(&ctrldata_pos, &input_pos, &output_pos, &setpoint_pos, kp_pos, ki_pos, kd_pos);
     // Set controler output limits from 0 to 200
-    pid_limits(pid_pos, 0, 4000);
+    pid_limits(pid_pos, 0, 66);
     // Allow PID to compute and change output
     pid_auto(pid_pos);
 
@@ -179,26 +192,5 @@ int main() {
     while (1)
     {
         tight_loop_contents();
-        // LCD
-        // printf("Pos: %d\n", enc_new);
-        // printf("dif: %d\n", enc_dif);
-        /* int2LCD("40", enc_new, 4);
-        float2LCD("14", enc2speed(enc_dif), 4);
-        printf("enc_diff: %d\n", enc_dif);
-        int2LCD("54", speed, 4);
-
-        // PID
-        float2LCD("4A", pid->lastin, 4);
-        float2LCD("22", output, 4); */
-
-        
-        // printf("Speed: %.2f, Pos: %d\r", enc2speed(enc_dif), enc_new); 
-        // float2LCD("14", enc2speed(enc_dif), 4);
-
-        // PWM        
-    /*     int pwm_value = new_value * 5;
-        speed = (speed >= 900) ? 900: speed;
-        pwm_set_chan_level(slice_num, PWM_CHAN_A, speed);
-        pwm_set_chan_level(slice_num, PWM_CHAN_B, 1024 - speed);  */
     }
 }
