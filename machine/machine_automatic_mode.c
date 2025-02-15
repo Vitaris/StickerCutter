e3a45da29a8e7d736bbf1103ce6e86bff41a3e9a
Vitaris <53018032+Vitaris@users.noreply.github.com>
2025-02-15 15:02:30 +0100

    Enhance automatic mode state machine with detailed comments and improved mark detection logic

--- machine/machine_automatic_mode.c
+++ machine/machine_automatic_mode.c
@@ -30,7 +30,7 @@ void activate_automatic_state() {
     monitor_data.sticker_height = 0.0;
     monitor_data.mark_distance = 0.0;
 
-    monitor_data.bottom_mark_detected = false;
+    monitor_data.sticker_dimensions_set = false;
 }
 
 void handle_automatic_state(void) {
@@ -69,13 +69,17 @@ void handle_automatic_state(void) {
         
         case AUTOMATIC_MARK_POS_REACHED:
             set_text_10(display.F2_text, "Na znacke");
-            servo_goto_delayed(machine.servo_1, FAR_AWAY_DISTANCE, AUTOMAT_SPEED_SLOW, HALF_SECOND_DELAY);
-            automatic_substate = AUTOMATIC_WAIT_FOR_CONSTANT_SPEED;
+            automatic_substate = AUTOMATIC_START_ROLLING_OUT_PAPER;
             break;
-
+            
 // ----------------------------------------------------------------------------------------------------------
 // Rolling paper at constant speed
-        case AUTOMATIC_WAIT_FOR_CONSTANT_SPEED:
+        case AUTOMATIC_START_ROLLING_OUT_PAPER:
+            servo_goto_delayed(machine.servo_1, FAR_AWAY_DISTANCE, AUTOMAT_SPEED_SLOW, HALF_SECOND_DELAY);
+            automatic_substate = AUTOMATIC_WAIT_FOR_CONSTANT_SPEED_OF_PAPER;
+            break;
+
+        case AUTOMATIC_WAIT_FOR_CONSTANT_SPEED_OF_PAPER:
             if (machine.servo_1->nominal_speed_reached) {
                 detector_restart();
                 automatic_substate = AUTOMATIC_WAITING_FOR_SAMPLES;
@@ -84,38 +88,37 @@ void handle_automatic_state(void) {
 
         case AUTOMATIC_WAITING_FOR_SAMPLES:
             if (detector.sampling_done) {
-                automatic_substate = AUTOMATIC_SCANNING;
+                automatic_substate = AUTOMATIC_SCANNING_FOR_MARK;
             }
             break;
 
-        case AUTOMATIC_SCANNING:
+        case AUTOMATIC_SCANNING_FOR_MARK:
             if (detect_mark()) {
-                automatic_substate = AUTOMATIC_FOUND;
+                automatic_substate = AUTOMATIC_MARK_FOUND;
                 // machine.servo_1->next_stop = (detector.mark_position + SENSOR_KNIFE_OFFSET_Y) / machine.servo_1->scale;
             }
             break;
 
 // ----------------------------------------------------------------------------------------------------------
 // Mark found, save positions and move to next step
-        case AUTOMATIC_FOUND:
+        case AUTOMATIC_MARK_FOUND:
             set_text_10(display.F2_text, "Zn Najdeny");
-            if (machine.servo_1->positioning == IDLE || true) {
+            if (monitor_data.sticker_dimensions_set) {
+                // Todo
+            } else {
                 if (monitor_data.first_mark_position == 0) {
                     automatic_substate = AUTOMATIC_SAVE_FIRST_MARK;
                 } else if (monitor_data.second_mark_position == 0) {
                     automatic_substate = AUTOMATIC_SAVE_SECOND_MARK;
                 } else if (monitor_data.third_mark_position == 0) {
-                    automatic_substate = AUTOMATIC_SAVE_THIRD_MARK;
-                } else if (monitor_data.bottom_mark_detected == false) {
-                    automatic_substate = AUTOMATIC_RETURN_TO_ZERO;
-                }
+                    automatic_substate = AUTOMATIC_SAVE_THIRD_MARK; }
             }
             break;
 
         // Will save a first mark position and withouth stopping will continue to search the next mark
         case AUTOMATIC_SAVE_FIRST_MARK:
             monitor_data.first_mark_position = detector.mark_position + SENSOR_KNIFE_OFFSET_Y;
-            automatic_substate = AUTOMATIC_WAIT_FOR_CONSTANT_SPEED;
+            automatic_substate = AUTOMATIC_WAIT_FOR_CONSTANT_SPEED_OF_PAPER;
             break;
         
         // Will save a second mark position, stops and waits for user to confirm the sticker height
@@ -127,7 +130,7 @@ void handle_automatic_state(void) {
             set_text_10(display.F2_text, "    Potvrd");
             if (machine.F2->state_raised) {
                 set_text_20(display.state_text_1, "Automat");
-                automatic_substate = AUTOMATIC_MARK_GOTO;
+                automatic_substate = AUTOMATIC_START_ROLLING_OUT_PAPER;
             }
             break;
 
@@ -140,6 +143,7 @@ void handle_automatic_state(void) {
             // set_text_20(display.state_text_2, "Potvrd vzdial. znac.");
             set_text_10(display.F2_text, "    Potvrd");
             if (machine.servo_1->positioning == IDLE && machine.F2->state_raised) {
+                monitor_data.sticker_dimensions_set = true;
                 automatic_substate = AUTOMATIC_GOTO_CUT_POSITION;
             }
             break;
@@ -197,33 +201,26 @@ void handle_automatic_state(void) {
             if (machine.servo_0->positioning == IDLE) {
                 knife_up();
                 servo_goto_delayed(machine.servo_0, machine.paper_mark_position, AUTOMAT_SPEED_FAST, HALF_SECOND_DELAY);
-                automatic_substate = AUTOMATIC_CUT_OPENING_SECTION;
+                servo_goto_delayed(machine.servo_1, (machine.servo_1->servo_position + (monitor_data.mark_distance / 2)), AUTOMAT_SPEED_FAST, HALF_SECOND_DELAY);
+                automatic_substate = AUTOMATIC_RETURN_NEW_DETECTION;
+                // Tu ma byt nieco ako hladat znovu znacku
             }
             break;
 
-
-
-
-
-
-
-
-
-        case AUTOMATIC_RETURN_TO_ZERO:
-            if (machine.servo_1->positioning == IDLE) {
-                servo_goto_delayed(machine.servo_1, 0.0, 15.0, HALF_SECOND_DELAY);
-                automatic_substate = AUTOMATIC_FINISHED;
+        case AUTOMATIC_RETURN_NEW_DETECTION:
+            if (machine.servo_0->positioning == IDLE && machine.servo_1->positioning == IDLE) {
+                automatic_substate = AUTOMATIC_WAIT_FOR_CONSTANT_SPEED_OF_PAPER;
             }
             break;
 
+// ----------------------------------------------------------------------------------------------------------
+// Should be used as stop case of the automatic mode
         case AUTOMATIC_FINISHED:
             automatic_substate = AUTOMATIC_IDLE;
             break;
     }
 
-    // handle_cutter_state();
     marks_monitor();
-
 }
 
 // This function should monitor distances between marks
@@ -233,7 +230,7 @@ static void marks_monitor(void) {
     static bool measuring = false;
     
     // Start measuring when first mark is detected
-    if (automatic_substate == AUTOMATIC_FOUND && !measuring) {
+    if (automatic_substate == AUTOMATIC_MARK_FOUND && !measuring) {
         if (first_mark) {
             last_mark_position = detector.mark_position;
             first_mark = false;
@@ -279,102 +276,4 @@ static void marks_monitor(void) {
 
 static void stop_knife_on_mark(void) {
     machine.servo_1->next_stop = (detector.mark_position + SENSOR_KNIFE_OFFSET_Y) / machine.servo_1->scale;
-}
-
-void handle_cutter_state(void) {
-    // Existing sticker_cut_compute function renamed and content moved here
-    switch(machine.cutter_state) {
-        case CUTTER_IDLE:
-            knife_up();
-            break;
-
-        case CUTTER_REQUESTED:
-            if (machine.servo_0->enc_position == 0.0) {
-                machine.cutter_state = AT_HOME;
-            } else {
-                machine.cutter_state = TO_HOME;
-            }
-            break;
-
-        case TO_HOME:
-            if (machine.servo_0->positioning == IDLE) {
-                knife_up();
-                servo_goto_delayed(machine.servo_0, 0.0, 10.0, HALF_SECOND_DELAY);
-            } 
-            else if (machine.servo_0->positioning == POSITION_REACHED) {
-                    machine.cutter_state = AT_HOME;
-            }
-            break;
-
-        case AT_HOME:
-            // Check if knife is above the mark
-            // if (get_next_stop(detector, machine.servo_1->enc_position) != machine.servo_1->enc_position) {
-            //     raise_error("Znacka nenajdena");
-            // }
-            // else {
-            //     machine.cutter_state = TO_PRECUT;
-            // }
-            break;
-
-        case TO_PRECUT:
-            if (machine.servo_0->positioning == IDLE) {
-                knife_up();
-                servo_goto_delayed(machine.servo_0, PRECUT_POSITION, AUTOMAT_SPEED_MID, HALF_SECOND_DELAY);
-            } else {
-                if (machine.servo_0->positioning == POSITION_REACHED) {
-                    machine.cutter_state = BACK_HOME;
-                }
-            }
-            break;
-
-        case BACK_HOME:
-            if (machine.servo_0->positioning == IDLE) {
-                knife_down();
-                servo_goto_delayed(machine.servo_0, 0.0, 10.0, HALF_SECOND_DELAY);
-            } else {
-                if (machine.servo_0->positioning == POSITION_REACHED) {
-                    machine.cutter_state = CUT_TO_END;
-                }
-            }
-            break;
-
-        case CUT_TO_END:
-            if (machine.servo_0->positioning == IDLE) {
-                servo_goto_delayed(machine.servo_0, CUT_LENGTH, AUTOMAT_SPEED_MID, HALF_SECOND_DELAY);
-            } else {
-                if (machine.servo_0->positioning == POSITION_REACHED) {
-                    machine.cutter_state = FINAL_RETURN;
-                }
-            }
-            break;
-
-        case FINAL_RETURN:
-            if (machine.servo_0->positioning == IDLE) {
-                servo_goto_delayed(machine.servo_0, 0.0, AUTOMAT_SPEED_MID, HALF_SECOND_DELAY);
-                knife_up();
-            } else {
-                if (machine.servo_0->positioning == POSITION_REACHED) {
-                    machine.cutter_state = CUT_DONE;
-                }
-            }
-            break;
-
-        case CUT_DONE:
-            machine.cutter_state = ROLL_OUT_PAPER;
-            break; 
-        
-        case ROLL_OUT_PAPER:
-            if (machine.servo_0->positioning == IDLE &&  machine.servo_1->positioning == IDLE) {
-                servo_goto_delayed(machine.servo_1, machine.servo_1->enc_position + 5.0, 5.0, HALF_SECOND_DELAY);
-            }
-            else if (machine.servo_1->positioning == POSITION_REACHED) {
-                machine.cutter_state = CUTTER_IDLE;
-            }
-            break;
-
-        case STOP_CUTTING:
-            machine.servo_0->positioning = IDLE;
-            machine.cutter_state = CUTTER_IDLE;
-            break;
-    }
 }
--- machine/machine_automatic_mode.h
+++ machine/machine_automatic_mode.h
@@ -7,43 +7,59 @@
 #include "../servo_motor/button.h"
 #include "../servo_motor/servo_motor.h"
 
+/**
+ * @brief Structure for monitoring and storing mark positions and sticker dimensions
+ * Used during the initialization phase to learn sticker dimensions and later
+ * for validating proper mark detection during operation
+ */
 typedef struct {
-    bool bottom_mark_detected;
-
-    float sticker_height;
-    float mark_distance;
-
-    float first_mark_position;
-    float second_mark_position;
-    float third_mark_position;
+    bool sticker_dimensions_set;          // Indicates if sticker dimensions are learned
+    
+    float sticker_height;                 // Height of a single sticker
+    float mark_distance;                  // Distance between stickers in a row
+    
+    float first_mark_position;            // Position of first detected mark
+    float second_mark_position;           // Position of second detected mark
+    float third_mark_position;            // Position of third detected mark
 } marks_monitor_t;
 
+/**
+ * @brief States for the automatic cutting mode state machine
+ * Controls the sequence of operations from mark detection to cutting
+ */
 typedef enum {
-    AUTOMATIC_IDLE,      
-    AUTOMATIC_MARK_GOTO,
-    AUTOMATIC_MARK_GOTO_IN_PROGRESS,
-    AUTOMATIC_MARK_POS_REACHED,
-    AUTOMATIC_WAIT_FOR_CONSTANT_SPEED,
-    AUTOMATIC_WAITING_FOR_SAMPLES,     
-    AUTOMATIC_SCANNING,     
-    AUTOMATIC_FOUND,
-    AUTOMATIC_SAVE_FIRST_MARK,
-    AUTOMATIC_SAVE_SECOND_MARK,
-    AUTOMATIC_SAVE_THIRD_MARK,
-    AUTOMATIC_GOTO_CUT_POSITION,
-    AUTOMATIC_WAIT_FOR_CUT_POSITION,
-    AUTOMATIC_CUT_OPENING_SECTION,
-    AUTOMATIC_CUT_OPENING_SECTION2,
-    AUTOMATIC_CUT_RETURN_TO_MARK,
-    AUTOMATIC_CUT_REST_SECTION,
-    AUTOMATIC_CUT_RETURN_TO_MARK_2,
-    AUTOMATIC_RETURN_TO_ZERO,     
-    AUTOMATIC_FINISHED
+    AUTOMATIC_IDLE,                       // Waiting for start command
+    AUTOMATIC_MARK_GOTO,                 // Moving knife to initial mark position
+    AUTOMATIC_MARK_GOTO_IN_PROGRESS,     // Waiting for knife to reach mark position
+    AUTOMATIC_MARK_POS_REACHED,          // Knife has reached mark position
+    AUTOMATIC_START_ROLLING_OUT_PAPER,   // Begin feeding paper
+    AUTOMATIC_WAIT_FOR_CONSTANT_SPEED_OF_PAPER, // Waiting for stable paper feed
+    AUTOMATIC_WAITING_FOR_SAMPLES,       // Collecting sensor readings
+    AUTOMATIC_SCANNING_FOR_MARK,         // Actively looking for marks
+    AUTOMATIC_MARK_FOUND,               // Mark detected
+    AUTOMATIC_SAVE_FIRST_MARK,          // Recording position of first mark
+    AUTOMATIC_SAVE_SECOND_MARK,         // Recording position of second mark
+    AUTOMATIC_SAVE_THIRD_MARK,          // Recording position of third mark
+    AUTOMATIC_GOTO_CUT_POSITION,        // Moving to cutting position
+    AUTOMATIC_WAIT_FOR_CUT_POSITION,    // Waiting to reach cutting position
+    AUTOMATIC_CUT_OPENING_SECTION,      // Starting the cut sequence
+    AUTOMATIC_CUT_OPENING_SECTION2,     // Continuing cut sequence
+    AUTOMATIC_CUT_RETURN_TO_MARK,       // Moving back to mark after first cut
+    AUTOMATIC_CUT_REST_SECTION,         // Cutting remaining section
+    AUTOMATIC_CUT_RETURN_TO_MARK_2,     // Final return to mark position
+    AUTOMATIC_RETURN_NEW_DETECTION,     // Preparing for next mark detection
+    AUTOMATIC_RETURN_TO_ZERO,           // Returning to home position
+    AUTOMATIC_FINISHED                  // Cycle complete
 } automatic_substate_t;
 
+/**
+ * @brief Initializes the automatic mode state machine
+ * Resets all monitoring data and sets initial state
+ */
 void activate_automatic_state();
 
+/**
+ * @brief Main processing function for automatic mode
+ * Handles state transitions and executes appropriate actions for each state
+ */
 void handle_automatic_state();
-
-void handle_cutter_state();
-
