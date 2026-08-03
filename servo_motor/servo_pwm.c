6f2ae33d31d5ed9a23c58ee66ba0a707c4f3d0fd
Vitaris <53018032+Vitaris@users.noreply.github.com>
2026-08-03 12:35:29 +0200

    Fix PWM clock divider initialization in pwm_chan_init function

--- servo_motor/servo_pwm.c
+++ servo_motor/servo_pwm.c
@@ -4,6 +4,7 @@
 
 #include "servo_pwm.h"
 
+#define CLK_DIV_20KHZ 6.103515625
 
 uint pwm_chan_init(uint gpio_pin_num){
     gpio_set_function(gpio_pin_num, GPIO_FUNC_PWM);
@@ -11,7 +12,7 @@ uint pwm_chan_init(uint gpio_pin_num){
 
     // Find out which PWM slice is connected to GPIO # (it's slice #)
     uint slice_num = pwm_gpio_to_slice_num(gpio_pin_num);
-    pwm_set_clkdiv(slice_num, gpio_pin_num); // PWM clock divider
+    pwm_set_clkdiv(slice_num, CLK_DIV_20KHZ); // PWM clock divider
     pwm_set_wrap(slice_num, 1023);  // Set period of 1024 cycles (0 to 1023 inclusive)
     pwm_set_enabled(slice_num, true);
     return slice_num;
